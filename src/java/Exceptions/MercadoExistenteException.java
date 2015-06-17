94f0f31406f9194f7b561302dece07938d64f05c
amaericanas <amaericanas@amaericanas-HP>
2015-06-16 23:04:31 -0300

    alteração simples

---
+++ src/java/Exceptions/MercadoExistenteException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class MercadoExistenteException extends Exception{
+   
+    
+     public MercadoExistenteException(){
+        super("Mercado já Existe");
+    }
+    
+ 
+}
